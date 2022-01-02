abfbe362ce33003c8da9ade4f9477f8979fc5817
Vivian <10968647+wasabibun@users.noreply.github.com>
2022-01-01 23:07:05 -0800

    rgb lighting change on layer useing layer_set_state_user

--- keyboards/keebio/iris/keymaps/wasabibun/config.h
+++ keyboards/keebio/iris/keymaps/wasabibun/config.h
@@ -19,3 +19,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
+#undef RGBLED_NUM
+//#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_SLEEP
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
--- keyboards/keebio/iris/keymaps/wasabibun/keymap.c
+++ keyboards/keebio/iris/keymaps/wasabibun/keymap.c
@@ -11,56 +11,58 @@ enum custom_keycodes
    LAYER2,
 };
 
-// Use https://config.qmk.fm/#/keebio/iris/rev4/LAYOUT to set up initial layout
-// We can convert the json to a .c file by using https://jhelvy.shinyapps.io/qmkjsonconverter/
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
+enum tapDances
 {
-   [_LAYER0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_HOME, KC_SPC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LGUI, KC_NO, TO(1), KC_SPC, KC_NO, KC_RALT),
-
-   [_LAYER1] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_END, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_PSCR, KC_LBRC, KC_RBRC, KC_NO, KC_SPC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PENT, KC_LPRN, KC_RPRN, TO(2), KC_SPC, KC_LCBR, KC_RCBR),
-
-   [_LAYER2] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, RGB_TOG, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, KC_LSFT, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS, KC_EQL, KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, KC_TRNS, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_TRNS, KC_SPC, KC_PLUS, KC_END, RGB_HUD, RGB_SAD, RGB_VAD, KC_ENT, KC_LPRN, KC_RPRN, TO(0), KC_SPC, KC_LBRC, KC_RBRC) 
+   TD_SHIFT_CAPS = 0,
+   TD_F1_RESET = 1
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+  [TD_F1_RESET] = ACTION_TAP_DANCE_DOUBLE(KC_F1, RESET)
 };
 
-void persistent_default_layer_set(uint16_t default_layer) 
-{
-   eeconfig_update_default_layer(default_layer);
-   default_layer_set(default_layer);
+// Use https://config.qmk.fm/#/keebio/iris/rev4/LAYOUT to set up initial layout
+// We can convert the json to a .c file by using https://jhelvy.shinyapps.io/qmkjsonconverter/
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_LAYER0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, TD(TD_SHIFT_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_HOME, KC_SPC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LGUI, MO(2), TO(1), KC_SPC, MO(1), KC_RALT),
+
+[_LAYER1] = LAYOUT(TD(TD_F1_RESET), KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_BSPC, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_END, KC_UNDS, KC_MINS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_LPRN, KC_RPRN, KC_NO, KC_SPC, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_NO, KC_PENT, KC_NO, KC_NO, TO(2), KC_SPC, KC_NO, KC_NO),
+
+[_LAYER2] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, RGB_TOG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_LSFT, KC_MPRV, KC_MNXT, KC_NO, KC_VOLU, KC_UNDS, KC_MINS, RGB_VAI, KC_NO, RGB_HUI, RGB_SAI, KC_NO, KC_MUTE, KC_MSTP, KC_MPLY, KC_NO, KC_VOLD, KC_EQL, KC_NO, KC_SPC, KC_PLUS, RGB_VAD, KC_NO, RGB_HUD, RGB_SAD, KC_ENT, KC_NO, KC_NO, TO(0), KC_SPC, KC_NO, KC_NO) 
+
+};
+
+void matrix_init_user(void) { // Runs boot tasks for keyboard
+#ifdef RGBLIGHT_ENABLE
+    // set the layer color at initalization, because it won't set otherwise
+    // rgblight_enable_noeeprom(); // enables Rgb, without saving settings
+    rgblight_enable();
+    rgblight_sethsv_noeeprom(250, 160, 255);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+2);
+#endif
 }
 
-void matrix_init_user(void) 
-{
-   rgblight_enable();
-}
-
-void matrix_scan_user(void) 
-{
-   #ifdef RGBLIGHT_ENABLE
-
-   static uint8_t old_layer = 255;
-   uint8_t new_layer = biton32(layer_state);
-
-   if (old_layer != new_layer) {
-      switch (new_layer) {
-      case _LAYER0:
-         rgblight_sethsv(HSV_CORAL);
-         break;
-      case _LAYER1:
-         rgblight_sethsv(HSV_TEAL);
-         break;
-      case _LAYER2:
-         rgblight_sethsv(HSV_PURPLE);
-         break;
-      }
-
-      old_layer = new_layer;
-  }
-
-  #endif //RGBLIGHT_ENABLE
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
+uint32_t layer_state_set_kb(uint32_t state) { //try _user?
+#ifdef RGBLIGHT_ENABLE
+    // change the color any time a layer switches
+    // This function is called every time a layer switches, no matter how it switches
+    switch (biton32(state)) {
+        case _LAYER1:
+            rgblight_sethsv_noeeprom(HSV_TURQUOISE);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+2);
+            break;
+        case _LAYER2:
+            rgblight_sethsv_noeeprom(HSV_PURPLE);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+2);
+            break;
+        default: // _LAYER0
+            rgblight_sethsv_noeeprom(250, 160, 255);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+2);
+            break;
+    }
+#endif
+   return state;  // this is required, DO NOT REMOVE
 }
 
 /*
